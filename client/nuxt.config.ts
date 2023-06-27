006db1372dc5e2fbf298ed7d64e99af7a966b38e
Attila Szőke <szokeattila9801@gmail.com>
2023-06-27 17:01:50 +0200

    style(client): Added prettier and eslint configs

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,11 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/eslint-module',
-    'nuxt-typed-router'
-  ],
+  modules: ["@nuxtjs/eslint-module", "nuxt-typed-router"],
   typescript: {
-    typeCheck: true
-  }
-})
+    typeCheck: true,
+  },
+});
