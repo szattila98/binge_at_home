c99d8438329500a86a8ace63d243e12ad60ce736
Attila Szőke <szokeattila9801@gmail.com>
2023-08-12 18:46:01 +0200

    feat(server): added typed pathing

--- server/src/api/health_check.rs
+++ server/src/api/health_check.rs
@@ -1,21 +1,13 @@
 use axum::{response::IntoResponse, Json};
+use axum_extra::routing::TypedPath;
 use serde::Serialize;
 use tracing::{info, instrument};
 
 use crate::{get_app_name, get_app_version};
 
-#[utoipa::path(
-    get,
-    path = "/api",
-    responses(
-        (status = 200, description = "Checks health of the service")
-    )
-)]
-#[instrument]
-pub async fn health_check() -> impl IntoResponse {
-    info!("health check called");
-    Json(HealthCheckResponse::default())
-}
+#[derive(TypedPath)]
+#[typed_path("/")]
+pub struct HealthCheckEndpoint;
 
 #[derive(Serialize)]
 struct HealthCheckResponse {
@@ -32,4 +24,17 @@ impl Default for HealthCheckResponse {
             app_version: get_app_version(),
         }
     }
+}
+
+#[utoipa::path(
+    get,
+    path = "/api",
+    responses(
+        (status = 200, description = "Checks health of the service")
+    )
+)]
+#[instrument(skip_all)]
+pub async fn health_check(_: HealthCheckEndpoint) -> impl IntoResponse {
+    info!("health check called");
+    Json(HealthCheckResponse::default())
 }
