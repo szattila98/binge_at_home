8751d1523fc0e0ad6c148becde6e7670d050a5c7
Attila Szőke <szokeattila9801@gmail.com>
2023-08-29 22:09:50 +0200

    test(server): tested crud commons

--- server/src/crud/catalog.rs
+++ server/src/crud/catalog.rs
@@ -2,12 +2,9 @@ use async_trait::async_trait;
 use sqlx::PgPool;
 use tracing::instrument;
 
-use crate::{
-    build_find_all_query,
-    model::{Catalog, EntityId},
-};
+use crate::model::{Catalog, EntityId};
 
-use super::{Entity, OrderBy, Pagination};
+use super::{build_find_all_query, Entity, OrderBy, Pagination};
 
 #[derive(Debug)]
 pub struct CreateCatalogRequest {
@@ -109,7 +106,7 @@ impl Entity<Self> for Catalog {
         ordering: Vec<OrderBy<CatalogOrdering>>,
         pagination: Option<Pagination>,
     ) -> Result<Vec<Self>, sqlx::Error> {
-        let query = build_find_all_query!("catalog", ordering, pagination);
+        let query = build_find_all_query("catalog", ordering, pagination);
 
         let catalogs = sqlx::query_as(&query).fetch_all(pool).await?;
 
