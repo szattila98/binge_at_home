a52921b501ccb359e63d30c18a8359963214daf0
Attila Szőke <szokeattila9801@gmail.com>
2023-09-13 22:57:34 +0200

    feat(server): added templated error handling to list-catalogs endpoint

--- server/src/api/fragments/list_catalogs.rs
+++ server/src/api/fragments/list_catalogs.rs
@@ -1,14 +1,9 @@
 use askama::Template;
-use axum::{
-    extract::{Query, State},
-    response::{IntoResponse, Response},
-};
+use axum::extract::{Query, State};
 use axum_extra::routing::TypedPath;
-use http::StatusCode;
 use serde::Serialize;
 use sqlx::PgPool;
-use thiserror::Error;
-use tracing::{debug, instrument};
+use tracing::{debug, error, instrument, warn};
 
 use crate::{
     crud::{catalog::CatalogSort, Entity, Pagination, Sort},
@@ -17,42 +12,52 @@ use crate::{
 
 #[derive(TypedPath)]
 #[typed_path("/catalog")]
-pub struct ListCatalogs;
+pub struct Endpoint;
+
+#[derive(Serialize)]
+enum TemplateState {
+    Success { catalogs: Vec<Catalog> },
+    NoCatalogsFound,
+    DbErr(String),
+}
 
 #[derive(Serialize, Template)]
 #[template(path = "fragments/list-catalogs.html")]
-struct ListCatalogsTemplate {
-    catalogs: Vec<Catalog>,
+pub struct PageTemplate {
+    state: TemplateState,
 }
 
-#[derive(Error, Debug)]
-pub enum ListCatalogsError {
-    #[error("database error")]
-    DbErr(#[from] sqlx::Error),
-}
-
-impl IntoResponse for ListCatalogsError {
-    fn into_response(self) -> Response {
-        let msg = self.to_string();
-        let status_code = match self {
-            ListCatalogsError::DbErr(_) => StatusCode::INTERNAL_SERVER_ERROR,
-        };
-        (status_code, msg).into_response()
+impl PageTemplate {
+    fn new(state: TemplateState) -> Self {
+        Self { state }
     }
 }
 
 #[instrument(skip(pool))]
 pub async fn list_catalogs(
-    _: ListCatalogs,
+    _: Endpoint,
     State(pool): State<PgPool>,
     pagination: Option<Query<Pagination>>,
     sort: Option<Query<Sort<CatalogSort>>>,
-) -> Result<impl IntoResponse, ListCatalogsError> {
-    // TODO error handling into template - no need for pub - add to snippets
+) -> PageTemplate {
     let pagination = pagination.map(|Query(p)| p);
     let sort = sort.map(|Query(o)| vec![o]).unwrap_or_else(|| vec![]);
-    let catalogs = Catalog::find_all(&pool, sort, pagination).await?;
-    let rendered = ListCatalogsTemplate { catalogs };
+
+    let result = Catalog::find_all(&pool, sort, pagination).await;
+    let Ok(catalogs) = result else {
+        let error = result.unwrap_err().to_string();
+        error!("database error: {error}");
+        return PageTemplate::new(TemplateState::DbErr(error));
+    };
+
+    if catalogs.is_empty() {
+        warn!("no catalogs found");
+        return PageTemplate::new(TemplateState::NoCatalogsFound);
+    };
+
+    let rendered = PageTemplate {
+        state: TemplateState::Success { catalogs },
+    };
     debug!("list catalogs rendered\n{rendered}");
-    Ok(rendered)
+    rendered
 }
--- server/src/database.rs
+++ server/src/database.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use anyhow::Context;
 use secrecy::ExposeSecret;
 use sqlx::{postgres::PgPoolOptions, PgPool};
@@ -10,6 +12,7 @@ pub async fn init(config: &Configuration, _: &Logger) -> anyhow::Result<PgPool> 
     info!("connecting to database...");
     let url = config.database().url();
     let pool = PgPoolOptions::new()
+        .acquire_timeout(Duration::from_secs(10))
         .connect(url.expose_secret())
         .await
         .context("could not establish connection to database on provided url")?;
