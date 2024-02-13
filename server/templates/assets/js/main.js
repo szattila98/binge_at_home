ae77925561ea972489c277957631c763eb4fabc3
CsalaPeter <csalapeter95@gmail.com>
2024-02-13 17:46:55 +0100

    feat(server): Hiding autosuggest list when needed

--- server/templates/assets/js/main.js
+++ server/templates/assets/js/main.js
@@ -1,7 +1,27 @@
-function closeAutosuggest() {
+/**
+ * @constructor
+ * @param {HTMLElement} element
+ */
+
+function closeAutosuggest(element) {
+    element = document.querySelector('.autosuggest-results')
     setTimeout(() => {
-        if (!(document.activeElement.id === 'search-input') && !document.activeElement.id.includes('search-result-') && !(document.activeElement.id === 'search-button')) { 
+        if (!(document.activeElement.id === 'search-input') && !document.activeElement.id.includes('search-result-') && !(document.activeElement.id === 'search-button') && !(document.activeElement.id === 'clear')) { 
             document.getElementById('autosuggest-results').innerHTML = '';
+            element.style.display = 'none'
         }
     }, 100)
+}
+
+/**
+ * @constructor
+ * @param {HTMLElement} element
+ * @param {number} inputElement 
+ */
+function hideSuggest(element, inputElement){
+    element = document.querySelector('.autosuggest-results')
+    inputElement = document.getElementById('search-input').constructor.length
+    if(inputElement <= 2){
+        element.style.display = 'none'
+    }
 }
